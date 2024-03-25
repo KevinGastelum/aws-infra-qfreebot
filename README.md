b78a31c47232f06a63081c7849e3d1ff9e94409a
KevinGastelum <kev.gas777@gmail.com>
2024-03-25 16:07:56 -0700

    Load CDK libs

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Welcome to your CDK TypeScript project
+# Steps to reproduce
+
+- Install (CDK) Cloud Dev Kit node command: `npm install -g aws-cdk`
+- Bootstrap your AWS acct details (only needs to be done once) run: `cdk bootstrap aws://UR_ACCT_NUMBER/UR_REGION{us-east-1}`
+  <br>(Get AWS acct details with `aws sts get-account-identity` from CLI)
+- Begin CDK project with: `cdk init -l typescript`
+- Edit file in - lib/UR_STACK_FILE_NAME.ts - Here is where we configure and create EC2 instance, Lambda, and Docker
+
+### Objective
+
+Deploy EC2 instance with all our settings already configured - VPC, IAM role, Security Group, Policy Rules, etc. Deploy Lambda Function with Docker that runs FastAPI, HTTPX asynchronously to deploy algo bot
+
+<!-- # Welcome to your CDK TypeScript project
 
 This is a blank project for CDK development with TypeScript.
 
@@ -11,4 +23,4 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npm run test`    perform the jest unit tests
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+* `npx cdk synth`   emits the synthesized CloudFormation template -->
--- lib/aws-infra-qfreebot-stack.ts
+++ lib/aws-infra-qfreebot-stack.ts
@@ -1,16 +1,133 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, aws_ec2, aws_iam } from 'aws-cdk-lib' // CfnOutput
+import { Construct } from 'constructs'
+import { Effect } from 'aws-cdk-lib/aws-iam'
+// import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as fs from 'fs'
 
-export class AwsInfraQfreebotStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+interface NetworkProps extends StackProps {
+  cidr: string
+}
 
-    // The code that defines your stack goes here
+export class NetworkStack extends Stack {
+  public readonly vpc: aws_ec2.Vpc
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsInfraQfreebotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  constructor(scope: Construct, id: string, props: NetworkProps) {
+    super(scope, id, props)
+
+    this.vpc = new aws_ec2.Vpc(this, 'VpcDemo', {
+      vpcName: 'VpcDemo',
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: 'PublicSubnet',
+          cidrMask: 24,
+          subnetType: aws_ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: 'PrivateSubnet',
+          cidrMask: 24,
+          subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          name: 'IsolatedSubnet',
+          cidrMask: 24,
+          subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    })
+    // Optional Add API Gateway - Try Adding Lambda Function here
+    // const dockerFunc = new lambda.DockerImageFunction(this, 'DockerFunc', {
+    //   code: lambda.DockerImageCode.fromImageAsset('../api'),
+    //   memorySize: 1024,
+    //   timeout: cdk.Duration.minutes(5),
+    //   architecture: lambda.Architecture.X86_64,
+    // })
+    //         const functionUrl = dockerFunc.addFunctionUrl({
+    //       authType: lambda.FunctionUrlAuthType.NONE,
+    //       cors: {
+    //         allowedMethods: [lambda.HttpMethod.ALL],
+    //         allowedHeaders: ["*"],
+    //         allowedOrigins: ["*"],
+    //       },
+    //     });
+
+    //     // Output the API function url.
+    //     new CfnOutput(this, "APIUrl", {
+    //       value: functionUrl.url,
+    //     });
+    //   }
+    // }
+  }
+}
+
+interface ApplicationProps extends StackProps {
+  keyName: string
+  vpc: aws_ec2.Vpc
+}
+
+export class ApplicationStack extends Stack {
+  constructor(scope: Construct, id: string, props: ApplicationProps) {
+    super(scope, id, props)
+
+    //  Security Group open 80 and 22
+    const sg = new aws_ec2.SecurityGroup(this, 'Open80And225G', {
+      securityGroupName: 'Open80And225G',
+      vpc: props.vpc,
+    })
+
+    sg.addIngressRule(
+      aws_ec2.Peer.anyIpv4(),
+      aws_ec2.Port.tcp(80),
+      'Open port 80 for http'
+    )
+
+    sg.addIngressRule(
+      aws_ec2.Peer.anyIpv4(),
+      aws_ec2.Port.tcp(22),
+      'Open port 22 for ssh'
+    )
+    // Role for Instance
+    const role = new aws_iam.Role(this, 'Ec2ToConnectSSMAndAccessS3', {
+      roleName: 'Ec2ToConnectSSMAndAccessS3',
+      assumedBy: new aws_iam.ServicePrincipal('ec2.amazonaws.com'),
+    })
+    role.addManagedPolicy(
+      aws_iam.ManagedPolicy.fromManagedPolicyArn(
+        this,
+        'SSMaanagedPolicyForEc2',
+        'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore'
+      )
+    )
+    role.addToPolicy(
+      new aws_iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: ['*'],
+        actions: ['s3:*'],
+      })
+    )
+
+    // EC2 in Public Subnet
+    const ec2 = new aws_ec2.Instance(this, 'PubEc2', {
+      instanceName: 'PubEc2',
+      vpc: props.vpc,
+      role: role,
+      securityGroup: sg,
+      vpcSubnets: {
+        subnetType: aws_ec2.SubnetType.PUBLIC,
+      },
+      instanceType: aws_ec2.InstanceType.of(
+        aws_ec2.InstanceClass.T3,
+        aws_ec2.InstanceSize.SMALL
+      ),
+      machineImage: new aws_ec2.AmazonLinuxImage({
+        generation: aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        edition: aws_ec2.AmazonLinuxEdition.STANDARD,
+      }),
+      allowAllOutbound: true,
+    })
+    //  Add user to Ec2
+    ec2.addUserData(
+      fs.readFileSync('./lib/user-data.sh', { encoding: 'utf-8' })
+    )
   }
 }
